f9e626ff9a25ff2f07ee26ffd8b393b26129727c
joseluisrnp <reciojoselu@gmail.com>
2023-11-19 18:30:47 +0100

    pwa config

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,6 +9,14 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/pwa-192x192.png" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00aba9" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="theme-color" content="#c11387" />
+        <link rel="manifest" href="/build/manifest.webmanifest">
+        <script id="vite-plugin-pwa:register-sw" src="/build/registerSW.js"></script>
 
         <!-- Scripts -->
         @routes
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import { fileURLToPath, URL } from 'node:url'
 
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
@@ -16,6 +16,45 @@ export default defineConfig({
                     base: null,
                     includeAbsolute: false,
                 },
+            },
+        }),
+        VitePWA({
+            outDir: 'public/build',
+            registerType: 'autoUpdate',
+            includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
+            workbox: {
+                cleanupOutdatedCaches: true,
+                directoryIndex: null,
+                globPatterns: ['**/*.{css,js,html,svg,png,ico,txt,woff2}'],
+                maximumFileSizeToCacheInBytes: 4194304,
+                navigateFallback: null,
+                navigateFallbackDenylist: [/\/[api,admin,livewire]+\/.*/],
+            },
+            manifest: {
+                name: 'ViveTuLinea',
+                short_name: 'ViveTuLinea',
+                theme_color: '#c11387',
+                id: '/',
+                scope: '/',
+                start_url: '/',
+                icons: [
+                    {
+                        src: '/pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable',
+                    },
+                ],
             },
         }),
     ],
