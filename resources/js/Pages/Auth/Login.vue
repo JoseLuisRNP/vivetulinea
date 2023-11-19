bbe8463db501abf30aac76fd8c6942cff9756951
joseluisrnp <reciojoselu@gmail.com>
2023-11-19 17:03:56 +0100

    initial commit

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import logo from '@/assets/logo.png'
+
+import {Head, Link, useForm, usePage} from '@inertiajs/vue3';
+import {watch} from "vue";
+
+defineProps<{
+    canResetPassword?: boolean;
+    status?: string;
+}>();
+
+const page = usePage();
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: true,
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => {
+            form.reset('password');
+        },
+    });
+};
+
+watch(() => page.props.errors, () => {
+    console.log(page.props.errors)
+})
+</script>
+
+<template>
+        <Head title="Log in" />
+
+        <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+            <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+                <img class="mx-auto h-16 w-auto mt-4" :src="logo" alt="ViveTuLinea">
+                <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"></h2>
+            </div>
+
+            <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                <form class="space-y-6" method="POST" @submit.prevent="submit">
+                    <div>
+                        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
+                        <div class="mt-2">
+                            <input v-model="form.email" id="email" name="email" type="email" autocomplete="email" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
+                    <div>
+                        <div class="flex items-center justify-between">
+                            <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Constraseña</label>
+                            <div class="text-sm">
+                                <a href="#" class="font-semibold text-secondary hover:text-secondary-focus">¿Olvidaste la contraseña?</a>
+                            </div>
+                        </div>
+                        <div class="mt-2">
+                            <input v-model="form.password" id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+                    <label class="label">
+                        <span v-if="page.props.errors.email" class="label-text-alt text-error">{{ page.props.errors.email}}</span>
+                    </label>
+                    <div>
+                        <button type="submit" class="btn btn-primary w-full">Inciar sesión</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,70 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                'primary-focus': '#AD0B71',
+                'secondary-focus': '#94B143',
+            },
+        },
+    },
+
+    plugins: [forms, require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                'light': {
+                    'primary' : '#c11387',
+                    'primary-focus' : '#AD0B71',
+                    'primary-content' : '#ffffff',
+
+                    'secondary' : '#9ec151',
+                    'secondary-focus' : '#94B143',
+                    'secondary-content' : '#ffffff',
+
+                    'accent' : '#37cdbe',
+                    'accent-focus' : '#2ba69a',
+                    'accent-content' : '#ffffff',
+
+                    'neutral' : '#3b424e',
+                    'neutral-focus' : '#2a2e37',
+                    'neutral-content' : '#ffffff',
+
+                    'base-100' : '#ffffff',
+                    'base-200' : '#f9fafb',
+                    'base-300' : '#ced3d9',
+                    'base-content' : '#1e2734',
+
+                    'info' : '#1c92f2',
+                    'success' : '#009485',
+                    'warning' : '#ff9900',
+                    'error' : '#ff5724',
+
+                    '--rounded-box': '1rem',
+                    '--rounded-btn': '.5rem',
+                    '--rounded-badge': '1.9rem',
+
+                    '--animation-btn': '.25s',
+                    '--animation-input': '.2s',
+
+                    '--btn-text-case': 'uppercase',
+                    '--navbar-padding': '.5rem',
+                    '--border-btn': '1px',
+                },
+            },
+        ],
+    },
+};
