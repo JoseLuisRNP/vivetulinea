11da25e0b2e5accd42a71f5e561a950f1bb3e722
joseluisrnp <reciojoselu@gmail.com>
2024-01-14 14:29:09 +0100

    change colors pautas

--- resources/js/Components/Pautas.vue
+++ resources/js/Components/Pautas.vue
@@ -6,7 +6,7 @@
         <h2 class="text-xl font-medium text-gray-500">Pautas saludables</h2>
         <ul role="list" class="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 sm:gap-6 lg:grid-cols-4">
             <li class="col-span-1 flex rounded-md shadow-sm">
-                <div class="flex w-16 flex-shrink-0 items-center justify-center bg-blue-600 rounded-l-md text-sm font-medium text-white">Agua</div>
+                <div class="flex w-16 flex-shrink-0 items-center justify-center bg-primary rounded-l-md text-sm font-medium text-white">Agua</div>
                 <div class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
                     <div class="flex-1 truncate px-4 text-sm">
                         <p class="font-medium text-xl text-gray-900 hover:text-gray-600">8 vasos</p>
@@ -22,7 +22,7 @@
                 </div>
             </li>
             <li class="col-span-1 flex rounded-md shadow-sm">
-                <div class="flex w-16 flex-shrink-0 items-center justify-center bg-green-500 rounded-l-md text-sm font-medium text-white">Verdura</div>
+                <div class="flex w-16 flex-shrink-0 items-center justify-center bg-primary rounded-l-md text-sm font-medium text-white">Verdura</div>
                 <div class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
                     <div class="flex-1 truncate px-4 text-sm">
                         <p class="font-medium text-xl text-gray-900 hover:text-gray-600">3 piezas</p>
@@ -30,7 +30,7 @@
                 </div>
             </li>
             <li class="col-span-1 flex rounded-md shadow-sm">
-                <div class="flex w-16 flex-shrink-0 items-center justify-center bg-red-600 rounded-l-md text-sm font-medium text-white">Deporte</div>
+                <div class="flex w-16 flex-shrink-0 items-center justify-center bg-primary rounded-l-md text-sm font-medium text-white">Deporte</div>
                 <div class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
                     <div class="flex-1 truncate px-4 text-sm">
                         <p class="font-medium text-xl text-gray-900 hover:text-gray-600">30 minutos</p>
