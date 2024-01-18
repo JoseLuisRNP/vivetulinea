8f9ba78e794266c8d4b682728cc32778e723b3d1
joseluisrnp <reciojoselu@gmail.com>
2024-01-18 17:01:35 +0100

    fix #17

--- resources/js/Components/Capital.vue
+++ resources/js/Components/Capital.vue
@@ -40,6 +40,14 @@ const user = usePage().props.auth.user;
                     </div>
                 </div>
             </li>
+            <li class="col-span-1 flex rounded-md shadow-sm">
+                <div class="flex w-16 flex-shrink-0 items-center justify-center bg-gray-600 rounded-l-md text-sm font-medium text-white">Extras</div>
+                <div class="flex flex-1 items-center justify-between truncate rounded-r-md border-b border-r border-t border-gray-200 bg-white">
+                    <div class="flex-1 truncate px-4 text-sm">
+                        <p class="font-medium text-xl text-gray-900 hover:text-gray-600">{{ user.weekly_points }}</p>
+                    </div>
+                </div>
+            </li>
         </ul>
     </div>
 
