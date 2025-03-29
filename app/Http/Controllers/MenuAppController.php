5ec2466098d53fae777bd2f21541b693f87b7b4c
JoseLuisRNP <reciojoselu@gmail.com>
2025-03-29 19:38:31 +0100

    fix

--- app/Http/Controllers/MenuAppController.php
+++ app/Http/Controllers/MenuAppController.php
@@ -12,12 +12,11 @@ class MenuAppController extends Controller
         $isSameDayOfRegister = now()->dayOfWeek === auth()->user()->created_at->addDay()->dayOfWeek;
         $lastWeight = auth()->user()->weights()->orderBy('date', 'desc')->first('date');
 
+        $shouldShowToast = false;
+
         if(auth()->user()->role === 'admin') { 
             $shouldShowToast = $isSameDayOfRegister && (!$lastWeight || !$lastWeight->date->isSameDay(now()));
         }
-
-        $shouldShowToast = $isSameDayOfRegister && !$lastWeight->date->isSameDay(now());
-
     
         return Inertia::render('ViewSelector', [
             'shouldShowToast' => $shouldShowToast
