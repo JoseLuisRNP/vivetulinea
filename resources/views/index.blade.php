a51edb23ad45f8d27306110248caa212f68d9732
JoseLuisRNP <reciojoselu@gmail.com>
2025-09-01 15:49:34 +0200

    update info

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -52,7 +52,7 @@
 
 
 <body>
-<a href="https://vivetulinea.es/campaign?referer=app-gratuita-mayo" target="_blank" class="app-banner">¡Prueba nuestra app gratis! <div class="app-banner-date">*Oferta disponible hasta el 25 de mayo</div></a>
+{{-- <a href="https://vivetulinea.es/campaign?referer=app-gratuita-mayo" target="_blank" class="app-banner">¡Prueba nuestra app gratis! <div class="app-banner-date">*Oferta disponible hasta el 25 de mayo</div></a> --}}
 <!-- Navigation
 ==========================================-->
 <nav id="tf-menu" class="navbar navbar-default navbar-fixed-top">
@@ -193,8 +193,8 @@
 
                     </ul>
 
-                    <a href="https://vivetulinea.es/campaign?referer=app-gratuita-mayo" target="_blank" class="btn btn-primary btn-vivatulinea" style="margin-top: 20px;">¡Prueba gratis ahora!</a>
-                    <p class="text-white" style="margin-top: 10px;">*Solo nuevos usuarios de ViveTuLinea, promoción valida hasta el 25 de mayo de 2025</p>
+                    {{-- <a href="https://vivetulinea.es/campaign?referer=app-gratuita-mayo" target="_blank" class="btn btn-primary btn-vivatulinea" style="margin-top: 20px;">¡Prueba gratis ahora!</a>
+                    <p class="text-white" style="margin-top: 10px;">*Solo nuevos usuarios de ViveTuLinea, promoción valida hasta el 25 de mayo de 2025</p> --}}
                 </div>
             </div>
         </div>
@@ -656,8 +656,8 @@
                 <article>
                     <h2>Teatinos</h2> CP: 29010 - Málaga Capital
                     <div>
-                        <red>* Cerrado por vacaciones de 1 de Agosto al 31 Agosto</red>
-                        <br/>
+                        {{-- <red>* Cerrado por vacaciones de 1 de Agosto al 31 Agosto</red> --}}
+                        {{-- <br/> --}}
                     </div>
                     <div class="block">
                         <span class="clear padding15"></span>
